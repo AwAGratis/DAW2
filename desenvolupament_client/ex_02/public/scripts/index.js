f6ec4d4605ca0ab036165dad052024a540f7d047
AwAGratis <guilleemrp2004@gmail.com>
2025-10-14 20:59:13 +0200

    Exercici 02 | Front-End

---
+++ desenvolupament_client/ex_02/public/scripts/index.js
@@ -0,0 +1,8 @@
+// Funció que executa els missatges
+const exerciciAlert = () => {
+    console.log("Hola em dic Guilem i aquest es el segon exercici de l'assignatura Front-End.")
+    alert("Hola em dic Guilem i aquest es el segon exercici de l'assignatura Front-End. Fes F12 per veure aquest missatge a consola.");
+};
+
+// Execució de la funció
+exerciciAlert();
